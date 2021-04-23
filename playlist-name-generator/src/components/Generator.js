9a7656cdf5c68c2596003bcc7457dacbf01e0b67
ashleighrae <67845170+ashleighrae@users.noreply.github.com>
2021-04-23 14:34:27 +1000

    Updated playlist names and functionality.

--- playlist-name-generator/src/components/Generator.js
+++ playlist-name-generator/src/components/Generator.js
@@ -5,9 +5,10 @@ const Generator = props => {
     const [values, setValue] = useState("");
 
     const handleClick = () => {
+        var regExp = /[a-zA-Z]/g;
+
         props.setKeywords(values);
-        
-        if (values != "") {
+        if (values !== "" && regExp.test(values)) {
             props.setGenerated(true);
         }
     }
@@ -15,7 +16,7 @@ const Generator = props => {
     return (
         <div className="Generator">
             <div class="form__group field">
-                <input type="input" onChange={event => setValue(event.target.value)} value={values} class="form__field" placeholder="Enter Main Keywords" name="keywords" id='keywords' required />
+                <input type="input" onChange={event => setValue(event.target.value)} value={values} class="form__field" placeholder="Seperate by Comma" name="keywords" id='keywords' required />
                 <label for="keywords" class="form__label">Enter Keywords</label>
 
                 <button class="testbutton" onClick={handleClick} />
